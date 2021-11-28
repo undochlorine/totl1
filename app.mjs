8fcda5f82d95eb49e3f99594c544f83978727c33
alex-sikach <hlophlop1245@gmail.com>
2021-11-28 22:14:59 +0200

    empty ready project

---
+++ app.mjs
@@ -0,0 +1,5 @@
+import security from "./security";
+import { Telegram } from "telegraf";
+import stickers from './stickers.js';
+import fetch from 'node-fetch';
+const bot = new Bot(security.TELEGRAM_BOT_TOKEN);
