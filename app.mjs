aadd178bb1e960ede6eb8481e62c033160c3d359
alex-sikach <hlophlop1245@gmail.com>
2022-02-15 00:50:24 +0200

    fetch time. остаthe rest is to fill logic validator

--- app.mjs
+++ app.mjs
@@ -1,43 +1,105 @@
 import security from './security.js';
 import { Telegraf } from 'telegraf';
 import stickers from './stickers.js';
-import fetch from 'node-fetch';
 import functions from './functions.js';
+import moment from "moment";
+import {readFileSync} from "fs";
+
 const bot = new Telegraf(security.TELEGRAM_BOT_TOKEN);
+
 const STATE_NORMAL = 'normal';
-const STATE_CLASS = 'waiting a class';
+const STATE_CLASS = 'waiting for a class';
+const WRONG_NUMBER = 'wrong number';
+const WRONG_LETTER = 'wrong letter'
+
 let state = STATE_NORMAL;
 let users_class = null, users_letter = null, users_grade = null;
 
 bot.telegram.setMyCommands([
-	{ command: '/set_class', description: 'Установить класс. Обязательная функция для пользования ботом.' }
+	{ command: '/start', description: 'Начать общение с ботом.' },
+	{ command: '/set_class', description: 'Установить класс. Обязательная функция для пользования ботом.' },
+	{ command: '/when_school_bell', description: 'Когда звонок?' },
+	{ command: '/timetable_today', description: 'Расписание на сегодня.' },
+	{ command: '/timetable_tomorrow', description: 'Расписание на завтра.' }
 ]);
 
 bot.on('message', async ctx =>
 {
-	const textLC = ctx.message.text.toLowerCase();
 	const text = ctx.message.text;
+	const textLC = text.toLowerCase();
 	const from = ctx.from;
 	const chatId = ctx.chat.id;
-	if(textLC === '/start')  {
+	if(textLC === '/start') {
 		await bot.telegram.sendMessage(chatId, `Привет, ${from.first_name}! В каком вы классе?`);
 		state = STATE_CLASS;
 		return 0;
 	} else if(textLC === '/set_class') {
 		state = STATE_CLASS;
-		return bot.telegram.sendMessage(chatId, `В каком вы классе?`);;
+		return bot.telegram.sendMessage(chatId, `В каком вы классе?`);
+	} else if(
+		textLC === '/when_school_bell' ||
+		(textLC.includes('когда') &&
+			(textLC.includes('урок') || textLC.includes('пара'))
+		)
+	) {
+		let json = await readFileSync('./fake_json/licey.json');
+		json = await JSON.parse(json);
+		//current mode of learning(online/offline)
+		let current = "offline";
+		if(json.stuff.timetable.offline.period[0] === null && json.stuff.timetable.online.period[0] !== null)
+			current = "online"
+		else if(json.stuff.timetable.offline.period[0] === null && json.stuff.timetable.online.period[0] === null)
+			current = null;
+		if(current === undefined)
+			return bot.telegram.sendMessage(chatId, 'Извините, у нас нету актуального расписания.')
+		let periodStart =
+			moment(json.stuff.timetable[current].period[0], 'DD.MM.YYYY')
+				.unix();
+		let periodEnd;
+		if(json.stuff.timetable[current].period[1] === null)
+			periodEnd = null
+		else
+			periodEnd =
+				moment(json.stuff.timetable[current].period[1], 'DD.MM.YYYY')
+					.unix();
+		let today =
+			moment(
+				`${moment().format('DD')}/${moment().format('MM')}/${moment().format('YYYY')}`,
+				'DD/MM/YYYY'
+				)
+				.unix();
+		//проверяем расписание на актуальность
+		console.log('period: ' + [periodStart, periodEnd === null]);
+		console.log('today: ' + today);
+		//todo: fill logic
+		if(periodEnd === null) {
+			if( today >= periodStart ) {
+				return bot.telegram.sendMessage(chatId, 'Актуальное расписание')
+			} else {
+				return bot.telegram.sendMessage(chatId, 'Устаревшее расписание')
+			}
+		} else if((today >= periodStart) && (today <= periodEnd)) {
+			if( today >= periodStart ) {
+				return bot.telegram.sendMessage(chatId, 'Актуальное расписание')
+			} else {
+				return bot.telegram.sendMessage(chatId, 'Устаревшее расписание')
+			}
+		}
+
 	}
+
 	if (state === STATE_CLASS) {
 		users_class = textLC;
 		state = STATE_NORMAL;
 		[users_grade, users_letter] = functions.valid_class(users_class);
-		if (users_grade === 'wrong number') {
+		if (users_grade === WRONG_NUMBER) {
 			return bot.telegram.sendMessage(chatId, 'Такого класса нет. Используйте команду /set_class чтобы попробовать ещё раз.')
-		} else if (users_letter === 'wrong letter') {
+		} else if (users_letter === WRONG_LETTER) {
 			return bot.telegram.sendMessage(chatId, 'Класса с такой буквой нет. Используйте команду /set_class чтобы попробовать ещё раз.');
 		} else if (users_letter === null && users_grade === null) {
+			return bot.telegram.sendMessage(chatId, `Вы ввели класс некоректно, повторите попытку, использую команду /set_class`);
 		} else {
-			return bot.telegram.sendMessage(chatId, `Окей. ваш класс: ${users_grade}${users_letter}`);
+			return bot.telegram.sendMessage(chatId, `Окей. ваш класс: ${users_grade}-${users_letter}`);
 		}
 	}
 
--- functions.js
+++ functions.js
@@ -1,47 +1,56 @@
-module.exports = {
-	valid_class: function (clas) {
-		let alph = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
-		let i, j;
-		let alphArray = alph.split('');
-		let thereareletter = Boolean(false);
-		alphArray.forEach((letter) => {
-			if (clas.includes(letter)) thereareletter = true;
-		});
-		if (!thereareletter)
-			return [null, null];
-		for (i = 0; i < clas.length; i++) {
-			if ( isNaN( Number( clas.charAt(i) ) ) ) break;
-		}
-		users_grade = clas.slice(0, i);
-		let users_letter = '';
-		clas = clas.slice(i);
+const alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+const alphabetArray = alphabet.split('');
+const validClassLetters = ['а', 'б', 'в', 'г', 'д', 'е'];
+const WRONG_NUMBER = 'wrong number';
+const WRONG_LETTER = 'wrong letter'
 
-		while (true && clas !== '') {
-			// console.log(`был clas: ${clas}`);
-			if (!alph.includes(clas[clas.length - 1])) {
-				// console.log('break');
-				break;
-			} else {
-				// console.log(`был ul: ${users_letter}`);
-				users_letter += clas[clas.length - 1];
-				// console.log(`стал ul: ${users_letter}`);
-				clas = clas.slice(0, (clas.length - 1));
-				// console.log(`стал clas: ${clas}`);
-			}
-		}
-		users_letter = users_letter.split('');
-		users_letter = users_letter.reverse();
-		users_letter = users_letter.join('');
-		console.log([users_grade, users_letter]);
-		if ((users_grade === '') || (users_letter === '')) {
-			return [null, null];
-		} else if (users_letter.length > 1) {
-			return [null, 'wrong letter']
-		} else if (Number(users_grade) > 11 || Number(users_grade) < 8) {
-			return ['wrong number', null];
+function valid_class(clas) {
+	let alph = alphabet;
+	let alphArray = alphabetArray;
+	let i, j;
+	let thereareletter = Boolean(false);
+	alphArray.forEach((letter) => {
+		if (clas.includes(letter)) thereareletter = true;
+	});
+	if (!thereareletter)
+		return [null, null];
+	for (i = 0; i < clas.length; i++) {
+		if ( isNaN( Number( clas.charAt(i) ) ) ) break;
+	}
+	users_grade = clas.slice(0, i);
+	let users_letter = '';
+	clas = clas.slice(i);
+
+	//убираем тире и всё такое
+	while (clas !== '') {
+		// console.log(`был clas: ${clas}`);
+		if (!alph.includes(clas[clas.length - 1])) {
+			// console.log('break');
+			break;
 		} else {
-			return [users_grade, users_letter];
+			// console.log(`был ul: ${users_letter}`);
+			users_letter += clas[clas.length - 1];
+			// console.log(`стал ul: ${users_letter}`);
+			clas = clas.slice(0, (clas.length - 1));
+			// console.log(`стал clas: ${clas}`);
 		}
 	}
+	users_letter = users_letter.split('');
+	users_letter = users_letter.reverse();
+	users_letter = users_letter.join('');
+	// console.log([users_grade, users_letter]);
+	if ((users_grade === '') || (users_letter === '')) {
+		return [null, null];
+	} else if (users_letter.length > 1 || (!validClassLetters.includes(users_letter))) {
+		return [null, WRONG_LETTER]
+	} else if (Number(users_grade) > 11 || Number(users_grade) < 8) {
+		return [WRONG_NUMBER, null];
+	} else {
+		return [users_grade, users_letter];
+	}
+}
+
+module.exports = {
+	valid_class: valid_class
 }
 
